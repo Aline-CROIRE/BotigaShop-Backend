c21e231510158f5aa67acab0dc680b2422c1974a
Aline NIYONIZERA <niyocroirealine@gmail.com>
2025-02-23 11:56:25 +0200

    Update swagger.js

--- swagger.js
+++ swagger.js
@@ -11,7 +11,7 @@ const options = {
     },
     servers: [
       {
-        url: 'https://botigashop-api.onrender.com/'
+        url: 'https://botigashop-api.onrender.com/',
         description: 'Development server',
       },
     ],
